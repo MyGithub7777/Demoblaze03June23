fff45162f3a174f915264bf401baeb4527a79ece
MyGithub7777 <asmygithub@gmail.com>
2023-10-11 19:50:01 +0530

    completed login task

--- DemoblazeProject/src/test/java/com/db/testLayer/LoginTest.java
+++ DemoblazeProject/src/test/java/com/db/testLayer/LoginTest.java
@@ -1,5 +1,13 @@
 package com.db.testLayer;
 
+import org.testng.annotations.Test;
+
 public class LoginTest {
 
+	
+	@Test
+	public void verifyLogin()
+	{
+		System.out.println("Verifed Login Test");
+	}
 }
